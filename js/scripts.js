9befb4a4123732082be7cb0feca387906712867f
damunza <danidamunza@gmail.com>
2018-07-05 08:33:36 +0300

    made comments

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@ $('document').ready(function(){
   $('.clickable').click(function(){
     $('.hidden').click(function(){
       $('img').show();
-    });
+    });//allows the image to show .hidden means the image was hidden
     $('.showing').click(function(){
       $('img').hide();
-    });
+    });//allows to hide the image .showing means the image was showing.
   });
 });
